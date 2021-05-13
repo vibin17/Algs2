12779e5174979b75342d51e4db62db796adb832d
Danila Grachev <grachev143d@gmail.com>
2021-05-14 03:27:30 +0600

    lr 5 and 6

---
+++ LR5/Program.cs
@@ -0,0 +1,547 @@
+﻿using System;
+
+public class Node
+{
+    public int data; // содержит ключ
+    public Node parent; // указатель на родителя
+    public Node left; // указатель на левого потомка
+    public Node right; // указатель на правого потомка
+    public int color; // 1 . Red, 0 . Black
+}
+
+
+public class Red_Black_tree
+{
+    private Node root;
+    private Node TNULL;
+
+    private void preOrderHelper(Node node)
+    {
+        if (node != TNULL)
+        {
+            Console.WriteLine(node.data + " ");
+            preOrderHelper(node.left);
+            preOrderHelper(node.right);
+        }
+    }
+
+    private void inOrderHelper(Node node)
+    {
+        if (node != TNULL)
+        {
+            inOrderHelper(node.left);
+            Console.WriteLine(node.data + " ");
+            inOrderHelper(node.right);
+        }
+    }
+
+    private void postOrderHelper(Node node)
+    {
+        if (node != TNULL)
+        {
+            postOrderHelper(node.left);
+            postOrderHelper(node.right);
+            Console.WriteLine(node.data + " ");
+        }
+    }
+
+    private Node searchTreeHelper(Node node, int key)
+    {
+        if (node == TNULL || key == node.data)
+        {
+            return node;
+        }
+
+        if (key < node.data)
+        {
+            return searchTreeHelper(node.left, key);
+        }
+        return searchTreeHelper(node.right, key);
+    }
+
+    // исправить дерево rb, измененное после операции удаления
+    private void fixDelete(Node x)
+    {
+        Node s;
+        while (x != root && x.color == 0)
+        {
+            if (x == x.parent.left)
+            {
+                s = x.parent.right;
+                if (s.color == 1)
+                {
+                    s.color = 0;
+                    x.parent.color = 1;
+                    leftRotate(x.parent);
+                    s = x.parent.right;
+                }
+
+                if (s.left.color == 0 && s.right.color == 0)
+                {
+                    s.color = 1;
+                    x = x.parent;
+                }
+                else
+                {
+                    if (s.right.color == 0)
+                    {
+                        s.left.color = 0;
+                        s.color = 1;
+                        rightRotate(s);
+                        s = x.parent.right;
+                    }
+                    s.color = x.parent.color;
+                    x.parent.color = 0;
+                    s.right.color = 0;
+                    leftRotate(x.parent);
+                    x = root;
+                }
+            }
+            else
+            {
+                s = x.parent.left;
+                if (s.color == 1)
+                {
+                    s.color = 0;
+                    x.parent.color = 1;
+                    rightRotate(x.parent);
+                    s = x.parent.left;
+                }
+
+                if (s.right.color == 0 && s.right.color == 0)
+                {
+                    s.color = 1;
+                    x = x.parent;
+                }
+                else
+                {
+                    if (s.left.color == 0)
+                    {
+                        s.right.color = 0;
+                        s.color = 1;
+                        leftRotate(s);
+                        s = x.parent.left;
+                    }
+                    s.color = x.parent.color;
+                    x.parent.color = 0;
+                    s.left.color = 0;
+                    rightRotate(x.parent);
+                    x = root;
+                }
+            }
+        }
+        x.color = 0;
+    }
+
+
+    private void rbTransplant(Node u, Node v)
+    {
+        if (u.parent == null)
+        {
+            root = v;
+        }
+        else if (u == u.parent.left)
+        {
+            u.parent.left = v;
+        }
+        else
+        {
+            u.parent.right = v;
+        }
+        v.parent = u.parent;
+    }
+
+    private void deleteNodeHelper(Node node, int key)
+    {
+        // найти узел, содержащий ключ
+        Node z = TNULL;
+        Node x, y;
+        while (node != TNULL)
+        {
+            if (node.data == key)
+            {
+                z = node;
+            }
+
+            if (node.data <= key)
+            {
+                node = node.right;
+            }
+            else
+            {
+                node = node.left;
+            }
+        }
+
+        if (z == TNULL)
+        {
+            Console.WriteLine("Couldn't find key in the tree");
+            return;
+        }
+
+        y = z;
+        int yOriginalColor = y.color;
+        if (z.left == TNULL)
+        {
+            x = z.right;
+            rbTransplant(z, z.right);
+        }
+        else if (z.right == TNULL)
+        {
+            x = z.left;
+            rbTransplant(z, z.left);
+        }
+        else
+        {
+            y = minimum(z.right);
+            yOriginalColor = y.color;
+            x = y.right;
+            if (y.parent == z)
+            {
+                x.parent = y;
+            }
+            else
+            {
+                rbTransplant(y, y.right);
+                y.right = z.right;
+                y.right.parent = y;
+            }
+
+            rbTransplant(z, y);
+            y.left = z.left;
+            y.left.parent = y;
+            y.color = z.color;
+        }
+        if (yOriginalColor == 0)
+        {
+            fixDelete(x);
+        }
+    }
+
+    // исправить красно-черное дерево
+    private void fixInsert(Node k)
+    {
+        Node u;
+        while (k.parent.color == 1)
+        {
+            if (k.parent == k.parent.parent.right)
+            {
+                u = k.parent.parent.left; // uncle
+                if (u.color == 1)
+                {
+                    u.color = 0;
+                    k.parent.color = 0;
+                    k.parent.parent.color = 1;
+                    k = k.parent.parent;
+                }
+                else
+                {
+                    if (k == k.parent.left)
+                    {
+                        k = k.parent;
+                        rightRotate(k);
+                    }
+                    k.parent.color = 0;
+                    k.parent.parent.color = 1;
+                    leftRotate(k.parent.parent);
+                }
+            }
+            else
+            {
+                u = k.parent.parent.right; // uncle
+
+                if (u.color == 1)
+                {
+                    u.color = 0;
+                    k.parent.color = 0;
+                    k.parent.parent.color = 1;
+                    k = k.parent.parent;
+                }
+                else
+                {
+                    if (k == k.parent.right)
+                    {
+                        k = k.parent;
+                        leftRotate(k);
+                    }
+                    k.parent.color = 0;
+                    k.parent.parent.color = 1;
+                    rightRotate(k.parent.parent);
+                }
+            }
+            if (k == root)
+            {
+                break;
+            }
+        }
+        root.color = 0;
+    }
+
+    private void printHelper(Node root, string indent, bool last)
+    {
+        // вывод дерева на экране
+        if (root != TNULL)
+        {
+            Console.WriteLine(indent);
+            if (last)
+            {
+                Console.WriteLine("R----");
+                indent += "     ";
+            }
+            else
+            {
+                Console.WriteLine("L----");
+                indent += "|    ";
+            }
+
+            string sColor = root.color == 1 ? "RED" : "BLACK";
+            Console.WriteLine(root.data + "(" + sColor + ")");
+            printHelper(root.left, indent, false);
+            printHelper(root.right, indent, true);
+        }
+    }
+
+    public Red_Black_tree()
+    {
+        TNULL = new Node();
+        TNULL.color = 0;
+        TNULL.left = null;
+        TNULL.right = null;
+        root = TNULL;
+    }
+
+    // Пре-ордер обход
+    // Node.Left Subtree.Right Subtree
+    public void preorder()
+    {
+        preOrderHelper(this.root);
+    }
+
+    // Обход по порядку
+    // Left Subtree . Node . Right Subtree
+    public void inorder()
+    {
+        inOrderHelper(this.root);
+    }
+
+    // Пост-ордер обход
+    // Left Subtree . Right Subtree . Node
+    public void postorder()
+    {
+        postOrderHelper(this.root);
+    }
+
+    // ищем в дереве ключ k
+    // и вернем соответствующий узел
+    public Node searchTree(int k)
+    {
+        return searchTreeHelper(this.root, k);
+    }
+
+    // находим узел с минимальным ключом
+    public Node minimum(Node node)
+    {
+        while (node.left != TNULL)
+        {
+            node = node.left;
+        }
+        return node;
+    }
+
+    // находим узел с максимальным ключом
+    public Node maximum(Node node)
+    {
+        while (node.right != TNULL)
+        {
+            node = node.right;
+        }
+        return node;
+    }
+
+    // находим преемника данного узла
+    public Node successor(Node x)
+    {
+        // если правое поддерево не нулевое,
+        // преемник - крайний левый узел в
+        // правое поддерево
+        if (x.right != TNULL)
+        {
+            return minimum(x.right);
+        }
+
+        // иначе это младший предок x, чей
+        // левый потомок также является предком x.
+        Node y = x.parent;
+        while (y != TNULL && x == y.right)
+        {
+            x = y;
+            y = y.parent;
+        }
+        return y;
+    }
+
+    // находим предшественника данного узла
+    public Node predecessor(Node x)
+    {
+        // если левое поддерево не равно нулю,
+        // предшественником является крайний правый узел в
+        // левое поддерево
+        if (x.left != TNULL)
+        {
+            return maximum(x.left);
+        }
+
+        Node y = x.parent;
+        while (y != TNULL && x == y.left)
+        {
+            x = y;
+            y = y.parent;
+        }
+
+        return y;
+    }
+
+    // повернуть влево в узле x
+    public void leftRotate(Node x)
+    {
+        Node y = x.right;
+        x.right = y.left;
+        if (y.left != TNULL)
+        {
+            y.left.parent = x;
+        }
+        y.parent = x.parent;
+        if (x.parent == null)
+        {
+            this.root = y;
+        }
+        else if (x == x.parent.left)
+        {
+            x.parent.left = y;
+        }
+        else
+        {
+            x.parent.right = y;
+        }
+        y.left = x;
+        x.parent = y;
+    }
+
+    // повернуть вправо в узле x
+    public void rightRotate(Node x)
+    {
+        Node y = x.left;
+        x.left = y.right;
+        if (y.right != TNULL)
+        {
+            y.right.parent = x;
+        }
+        y.parent = x.parent;
+        if (x.parent == null)
+        {
+            this.root = y;
+        }
+        else if (x == x.parent.right)
+        {
+            x.parent.right = y;
+        }
+        else
+        {
+            x.parent.left = y;
+        }
+        y.right = x;
+        x.parent = y;
+    }
+
+    // вставьте ключ к дереву в соответствующее место
+    // и исправляем дерево
+    public void insert(int key)
+    {
+        // Вставка обычного двоичного поиска (OBSI)
+        Node node = new Node();
+        node.parent = null;
+        node.data = key;
+        node.left = TNULL;
+        node.right = TNULL;
+        node.color = 1; // новый узел должен быть красным
+
+        Node y = null;
+        Node x = this.root;
+
+        while (x != TNULL)
+        {
+            y = x;
+            if (node.data < x.data)
+            {
+                x = x.left;
+            }
+            else
+            {
+                x = x.right;
+            }
+        }
+
+        // y является родителем x
+        node.parent = y;
+        if (y == null)
+        {
+            root = node;
+        }
+        else if (node.data < y.data)
+        {
+            y.left = node;
+        }
+        else
+        {
+            y.right = node;
+        }
+
+        // если новый узел является корневым, просто return
+        if (node.parent == null)
+        {
+            node.color = 0;
+            return;
+        }
+
+        // если дедушка и бабушка равны нулю, просто return
+        if (node.parent.parent == null)
+        {
+            return;
+        }
+
+        // исправление дерева
+        fixInsert(node);
+    }
+
+    public Node getRoot()
+    {
+        return this.root;
+    }
+
+    // удалить узел из дерева
+    public void deleteNode(int data)
+    {
+        deleteNodeHelper(this.root, data);
+    }
+
+    // вывод дерева
+    public void prettyPrint()
+    {
+        printHelper(this.root, "", true);
+    }
+
+    public static void Main(String[] args)
+    {
+        Red_Black_tree bst = new Red_Black_tree();
+        bst.insert(8);
+        bst.insert(18);
+        bst.insert(5);
+        bst.insert(15);
+        bst.insert(17);
+        bst.insert(25);
+        bst.insert(40);
+        bst.insert(80);
+        bst.deleteNode(25);
+        bst.prettyPrint();
+    }
+}
